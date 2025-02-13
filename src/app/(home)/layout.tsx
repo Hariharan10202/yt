3c5adc2d65569dec39d9c7489122a93bab0263bd
Hariharan <hariharan10202@gmail.com>
2025-02-13 09:13:59 -0500

    first commit

---
+++ src/app/(home)/layout.tsx
@@ -0,0 +1,8 @@
+import { PropsWithChildren } from "react";
+import HomeLayout from "@/modules/home/ui/layouts/home-layout";
+
+const Layout = ({ children }: PropsWithChildren) => {
+  return <HomeLayout>{children}</HomeLayout>;
+};
+
+export default Layout;
